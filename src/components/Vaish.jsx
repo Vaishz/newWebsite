fe81213d45f0d5b58d36334fc68ed53baf6673b5
Dhruvt0310 <taterdhruv0310@gmail.com>
2024-02-13 19:19:03 +0530

    hehhe

--- src/components/Vaish.jsx
+++ src/components/Vaish.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Vaish = () => {
   return (
     <div>
-        <h2>AJFSDSG</h2>
+        <h2>Dhruv is the best
+          Swer sucksssssss doesnt come to play on time
+          fu vaish
+        </h2>
     </div>
   )
 }
